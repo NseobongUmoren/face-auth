e7e4c54e2f70066cb3bbf96b9b6d425f229603ca
Nseobong Umoren <nseobongumoren@gmail.com>
2023-10-11 06:51:35 +0100

    ensure use of ssl/tls

--- db.ts
+++ db.ts
@@ -1,7 +1,7 @@
 import { createPool } from 'mysql2/promise';
 
 const dbConfig = {
-  host: 'aws.connect.psdb.cloud',
+  host: 'https://aws.connect.psdb.cloud',
   user: '4kamkooi3ijukrd3z3on',
   password: 'pscale_pw_4t6kbzvc13dTuw62phopKUPfTu1WEljtxTOuN8BZXzh',
   database: 'face-auth',
