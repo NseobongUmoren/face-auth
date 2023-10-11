a776368d367626166be71ed78b76a50cce2ec866
Nseobong Umoren <nseobongumoren@gmail.com>
2023-10-11 07:59:48 +0100

    debugging ssl

--- db.ts
+++ db.ts
@@ -1,13 +1,18 @@
 import { createPool } from 'mysql2/promise';
+import fs from 'fs'; // Node.js file system module
 
 const dbConfig = {
   host: 'aws.connect.psdb.cloud',
-  user: '4kamkooi3ijukrd3z3on',
-  password: 'pscale_pw_4t6kbzvc13dTuw62phopKUPfTu1WEljtxTOuN8BZXzh',
+  user: 'zq5k01fngh0s84whq87n',
+  password: 'pscale_pw_XLGQwWOwvIYowNlo9aBl5bDQWs9U5gteQhq32hPxStg',
   database: 'face-auth',
-  port: 3306
+  port: 3306,
+  ssl: {
+    ca: fs.readFileSync('/etc/pki/tls/certs/ca-bundle.crt'), // Replace with the path to your CA certificate
+  },
 };
 
 const pool = createPool(dbConfig);
 
 export default pool;
+
