2ca0a90d37e4f85d1090eab14d76bb97aef14f74
Nseobong Umoren <nseobongumoren@gmail.com>
2023-10-11 06:48:10 +0100

    changed database

--- db.ts
+++ db.ts
@@ -1,10 +1,10 @@
 import { createPool } from 'mysql2/promise';
 
 const dbConfig = {
-  host: 'https://redemptionfm.com',
-  user: 'redempt2_faceauth',
-  password: 'redempt2_faceauth',
-  database: 'redempt2_faceauth',
+  host: 'aws.connect.psdb.cloud',
+  user: '4kamkooi3ijukrd3z3on',
+  password: 'pscale_pw_4t6kbzvc13dTuw62phopKUPfTu1WEljtxTOuN8BZXzh',
+  database: 'face-auth',
   port: 3306
 };
 
